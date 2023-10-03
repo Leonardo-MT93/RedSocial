7822bbb7d7387294ca66695aaf107f6f2736a029
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2023-10-03 16:42:38 -0300

    Updating Database and relocation to Redsocial

--- client/database/connection.js
+++ client/database/connection.js
@@ -1,7 +1,23 @@
+// const mongoose = require("mongoose");
+// const connection = async () => {
+//     try{
+//         await mongoose.connect("mongodb://0.0.0.0:27017/mi_redsocial");
+//         console.log("Conectado correctamente a BD: mi_redsocial");
+//     }catch(error){
+//         console.log(error);
+//         throw new Error("Nose ha podido conectar a la base de datos");
+//     }
+// }
+
+// module.exports = connection
+
 const mongoose = require("mongoose");
 const connection = async () => {
     try{
-        await mongoose.connect("mongodb://0.0.0.0:27017/mi_redsocial");
+        await mongoose.connect('mongodb+srv://redsocial:WYm8pmMDXb5DX7J5@cluster0.0x1u4oe.mongodb.net/RedSocial',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        });
         console.log("Conectado correctamente a BD: mi_redsocial");
     }catch(error){
         console.log(error);
@@ -10,3 +26,4 @@ const connection = async () => {
 }
 
 module.exports = connection
+
